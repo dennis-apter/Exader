9ffdc89df23ad1b76ceccc0e5a1b6fc7f5bbf5d5
Dennis Apter <df-46@live.ru>
2016-02-22 15:07:05 +0500

    Published to nuget.org

--- src/Exader.IO.Shared/Properties/AssemblyInfo.cs
+++ src/Exader.IO.Shared/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Exader Framework")]
 [assembly: AssemblyCopyright("Copyright © Dennis Apter 2008-2016")]
 
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
